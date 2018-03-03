16aa5415772602303ca4530611b3284c3c4c1ab7
David <dtonlai@ualberta.ca>
2018-03-03 12:10:30 -0700

    first

---
+++ learning_users/basic_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from basic_app.models import UserProfileInfo
+
+admin.site.register(UserProfileInfo)
+
+#dtonlai -dtonlai@ualberta.ca -testpassword1
+
+# Register your models here.
